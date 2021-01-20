2ba622e0ac27091d079d758668a048b955b4024d
JackalGame <77704393+JackalGame@users.noreply.github.com>
2021-01-20 19:46:44 +0000

    Add files via upload

---
+++ EnemyShip.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using UnityEngine;
+
+
+public class EnemyShip : MonoBehaviour
+{
+    [Header("General")]
+    [SerializeField] string alienName;
+    [Range(0f, 10f)] [SerializeField] float speed = 1f;
+    [SerializeField] int enemyHealth = 1;
+    [SerializeField] float bonusScore = 50f;
+    [Header("Projectile")]
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] GameObject gun;
+    [SerializeField] float projectileSpeed = 1f;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 2f;
+    [Header("Effects")]
+    [SerializeField] AudioClip deathSFX;
+    [SerializeField] AudioClip laserSFX;
+    [SerializeField] GameObject explosionVFX;
+    [SerializeField] float explosionDuration = 1f;
+    
+
+    private Rigidbody2D rigidbody;
+    private Vector2 screenBounds;
+
+    float shotCounter;
+    float sfxVolume;
+
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = new Vector2(-speed, 0f);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        sfxVolume = PlayerPrefsController.GetMasterSFXVolume();
+    }
+
+
+    private void Update()
+    {
+        if (transform.position.x < screenBounds.x * -2)
+        {
+            Destroy(this.gameObject);
+        }
+
+        CountDownAndShoot(); 
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        if (transform.position.x < screenBounds.x)
+        {
+            GameObject laser = Instantiate(projectilePrefab, gun.transform.position, transform.rotation) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(-projectileSpeed, 0);
+            AudioSource.PlayClipAtPoint(laserSFX, Camera.main.transform.position, sfxVolume);
+        }            
+    }
+
+    public void DealDamage(int damage)
+    {
+        enemyHealth -= damage;
+        if(enemyHealth <= 0)
+        {
+            UnlockAlien();
+            Destroy(gameObject);
+            AudioSource.PlayClipAtPoint(deathSFX, Camera.main.transform.position, sfxVolume * 2);
+            GameObject explosion = Instantiate(explosionVFX, transform.position, transform.rotation);
+            Destroy(explosion, explosionDuration);
+            FindObjectOfType<GameManager>().AddBonusScore(bonusScore);
+            AddStars();
+        }
+    }
+
+    public void UnlockAlien() // Unlock Alien on Alien Data Scene.
+    {
+        int aliensKilled = 1;
+
+        if (aliensKilled >= PlayerPrefs.GetInt(alienName, 0))
+        {
+            PlayerPrefs.SetInt(alienName, 1);
+        }
+    }
+
+    public void AddStars()
+    {
+        int totalStars = PlayerPrefs.GetInt("Stars", 0);
+        totalStars += 2;
+        PlayerPrefs.SetInt("Stars", totalStars);
+    }
+}
---
+++ Spawner.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject asteroidPrefab;
+    [SerializeField] float firstWaveAsteroidMinSpawnTime = 0.3f;
+    [SerializeField] float firstWaveAsteroidMaxSpawnTime = 1.5f;
+    [SerializeField] float secondWaveAsteroidMinSpawnTime = 0.3f;
+    [SerializeField] float secondWaveAsteroidMaxSpawnTime = 1.5f;
+    [SerializeField] float thirdWaveAsteroidMinSpawnTime = 0.3f;
+    [SerializeField] float thirdWaveAsteroidMaxSpawnTime = 1.5f;
+
+    [Header("Enemy Wave")]
+    [SerializeField] float enemyMinSpawnTime = 5f;
+    [SerializeField] float enemyMaxSpawnTime = 10f;
+    [SerializeField] int firstScoreCheckpoint = 1000;
+    [SerializeField] int secondScoreCheckpoint = 2000;
+    [SerializeField] GameObject[] firstEnemyPrefabArray;
+    [SerializeField] GameObject[] secondEnemyPrefabArray;
+    [SerializeField] GameObject[] thirdEnemyPrefabArray;
+
+
+
+    [Header("Shredders")]
+    [SerializeField] GameObject leftLaserShredderPrefab;
+    [SerializeField] GameObject rightLaserShredderPrefab;
+
+    private Vector2 screenBounds;
+    int score;
+
+    private void Awake()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    void Start()
+    {
+        SpawnLaserShredders();
+        StartCoroutine(AsteroidWave());
+        StartCoroutine(EnemyWave());
+    }
+
+    private void Update()
+    {
+        score = FindObjectOfType<GameManager>().totalScoreAmount;
+    }
+
+    private void SpawnLaserShredders()
+    {
+        GameObject rightLaserShredder = Instantiate(rightLaserShredderPrefab) as GameObject;
+        rightLaserShredder.transform.position = new Vector2(screenBounds.x + 0.3f, screenBounds.y - 5);
+
+        GameObject leftLaserShredder = Instantiate(leftLaserShredderPrefab) as GameObject;
+        leftLaserShredder.transform.position = new Vector2(-screenBounds.x - 0.3f, screenBounds.y - 5);
+    }
+
+    
+
+    IEnumerator AsteroidWave()
+    {
+        while (true)
+        {
+            if (score < firstScoreCheckpoint) 
+            {
+                var asteroidRespawnTime = Random.Range(firstWaveAsteroidMinSpawnTime, firstWaveAsteroidMaxSpawnTime);
+                yield return new WaitForSeconds(asteroidRespawnTime);
+                SpawnAsteroid();
+            }
+            else if (score >= firstScoreCheckpoint && score < secondScoreCheckpoint)
+            {
+                var asteroidRespawnTime = Random.Range(secondWaveAsteroidMinSpawnTime, secondWaveAsteroidMaxSpawnTime);
+                yield return new WaitForSeconds(asteroidRespawnTime);
+                SpawnAsteroid();
+            }
+            else
+            {
+                var asteroidRespawnTime = Random.Range(thirdWaveAsteroidMinSpawnTime, thirdWaveAsteroidMaxSpawnTime);
+                yield return new WaitForSeconds(asteroidRespawnTime);
+                SpawnAsteroid();
+            }
+        }
+    }
+
+    private void SpawnAsteroid()
+    {
+        GameObject asteroid = Instantiate(asteroidPrefab) as GameObject;
+        asteroid.transform.position = new Vector2(screenBounds.x * 2, Random.Range(-screenBounds.y, screenBounds.y));
+    }
+
+   
+    IEnumerator EnemyWave()
+    {
+        while (true)
+        {
+            var enemyRespawnTime = Random.Range(enemyMinSpawnTime, enemyMaxSpawnTime);
+            yield return new WaitForSeconds(enemyRespawnTime);
+            SpawnEnemy();
+        }
+    }
+    private void SpawnEnemy()
+    {
+        if (score < firstScoreCheckpoint)
+        {
+            var firstEnemyIndex = Random.Range(0, firstEnemyPrefabArray.Length);
+            Spawn(firstEnemyPrefabArray[firstEnemyIndex]);
+        }
+        else if (score >= firstScoreCheckpoint && score < secondScoreCheckpoint)
+        {
+            var secondEnemyIndex = Random.Range(0, secondEnemyPrefabArray.Length);
+            Spawn(secondEnemyPrefabArray[secondEnemyIndex]);
+        }
+        else
+        {
+            var thirdEnemyIndex = Random.Range(0, thirdEnemyPrefabArray.Length);
+            Spawn(thirdEnemyPrefabArray[thirdEnemyIndex]);
+        }
+    }
+    
+    private void Spawn (GameObject myEnemy)
+    {
+        GameObject enemy = Instantiate(myEnemy) as GameObject;
+        enemy.transform.position = new Vector2(screenBounds.x * 2, Random.Range(-screenBounds.y + 1, screenBounds.y - 1));
+    }
+}
